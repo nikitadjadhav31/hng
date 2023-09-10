20b00195a9337764ce0575d4fab0d4c81562c704
NIkita Dilip Jadhav <96189626+nikitadjadhav31@users.noreply.github.com>
2023-09-10 21:07:17 +0530

    Update BackendStageTask1Application.java

--- BackendStageTask1Application.java
+++ BackendStageTask1Application.java
@@ -1,15 +1,60 @@
 package com.task1.hng;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BackendStageTask1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendStageTask1Application.class, args);
+    }
+}
+
+@RestController
+class MyController {
+
+    @GetMapping("/api")
+    public Map<String, Object> myEndpoint(
+            @RequestParam(name = "slack_name") String slackName,
+            @RequestParam(name = "track") String track) {
+
+        // Check if slack_name and track parameters are provided
+        if (slackName == null || track == null) {
+            throw new IllegalArgumentException("Both slack_name and track are required");
+        }
+
+        // Get the current day of the week
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        String dayOfWeek = new SimpleDateFormat("EEEE").format(calendar.getTime());
+
+        // Get the current UTC time
+        String currentTime = new SimpleDateFormat("HH:mm:ss").format(calendar.getTime());
+
+        // Construct the GitHub URLs based on your project
+        String githubRepoUrl = "https://github.com/nikitadjadhav31/hng";
+        String githubFileUrl = githubRepoUrl + "/blob/main/BackendStageTask1Application.java";
+
+        // Create the response JSON
+        Map<String, Object> response = new HashMap<>();
+        response.put("slack_name", slackName);
+        response.put("day_of_week", dayOfWeek);
+        response.put("current_utc_time", currentTime);
+        response.put("track", track);
+        response.put("github_file_url", githubFileUrl);
+        response.put("github_repo_url", githubRepoUrl);
+        response.put("status_code", 200);
+
+        return response;
     }
 }
 
